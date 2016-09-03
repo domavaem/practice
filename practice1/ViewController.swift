42077ee8e00d954546c16bced761bbd83fac4ccf
eso l <leso@lesoui-imac.local>
2016-09-03 16:55:06 +0900

    Initial Commit

---
+++ practice1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  practice1
+//
+//  Created by leso on 2016. 9. 3..
+//  Copyright © 2016년 leso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
