2465daeaad7f7659d6721348eebd970b6a5661f2
leso <test@testtt.com>
2016-09-04 22:46:21 +0900

    no message

---
+++ practice1/testCommit.swift
@@ -0,0 +1,13 @@
+//
+//  testCommit.swift
+//  practice1
+//
+//  Created by leso on 2016. 9. 4..
+//  Copyright © 2016년 leso. All rights reserved.
+//
+
+import UIKit
+
+class testCommit: NSObject {
+
+}
